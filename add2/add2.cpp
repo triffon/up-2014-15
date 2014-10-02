685e36e4dff18e93028249f4695c94b22caeb16c
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2014-10-02 17:48:42 +0300

    Втора програма на C++: добавяне на две числа.

---
+++ add2/add2.cpp
@@ -0,0 +1,20 @@
+/*
+ * add2.cpp
+ *
+ *  Created on: 2.10.2014 г.
+ *      Author: trifon
+ */
+
+
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b;
+  // първо въвеждаме стойности
+  cout << "a = "; cin >> a;
+  cout << "b = "; cin >> b;
+  int c = a + b; // събираме числата
+  cout << "a + b = " << c << endl;
+  return 0;
+}
+
