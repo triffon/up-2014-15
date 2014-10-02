064f3ba1785f6ff89d01ebd39a09b7b62f3aa2e1
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2014-10-02 17:43:28 +0300

    Първа програма на C++.

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// elms.fmi.uni-sofia.bg
+int main() {
+  int a = 4123;
+  cout << "a = " << a << endl;
+  cout << "2a = " << 2 * a << endl;
+  return 0;
+}
+
+
+
