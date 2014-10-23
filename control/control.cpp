4ddd047fe67f004393a1bbbe6d5d512d10c8a5ea
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2014-10-23 13:30:06 +0200

    Примерни програми за условен оператор (if) и оператор за многозначен избор (switch).

--- control/control.cpp
+++ control/control.cpp
@@ -52,16 +52,146 @@ int lineq() {
 			cout << "Никое x не е решение!" << endl;
 	else {
 		// ако a != 0
+		double a = 10;
 		double x = -b / a;
 		cout << "Решението е " << x << endl;
 	}
+	// !!! cout << "Отново: решението е " << x << endl;
+
+	cout << "a = " << a << endl;
+	// double a = 5;
 
 	return 0;
 }
 
+int numbers3() {
+	int a, b, c;
+	cout << "a, b, c = "; cin >> a >> b >> c;
+
+	cout << "Числата ";
+
+	/*
+	if (b > a && c > b)
+		;
+	else
+		cout << "НЕ ";
+	 */
+
+	if (b <= a || c <= b)
+		cout << "НЕ ";
+	cout << "образуват растяща редица." << endl;
+	return 0;
+}
+
+int min3() {
+	int a, b, c;
+	cout << "a, b, c = "; cin >> a >> b >> c;
+	int min = a;
+	// за момента a е най-малкото число от тези,
+	// които сме прегледали
+
+	/*
+	if (a <= b && a <= c)
+		min = a;
+	if (b <= a && b <= c)
+		min = b;
+	if (c <= a && c <= b)
+		min = c;
+	*/
+
+	// преглеждаме b:
+	if (b < min)
+		// има по-малко число: b
+		min = b;
+
+	// до момента min е най-малкото число от a и от b
+
+	// преглеждаме c:
+	if (c < min)
+		// има по-малко число: c
+		min = c;
+
+	// до момента min е най-малкото число от a, b, c
+
+	cout << "Най-малкото число е " << min << endl;
+	return 0;
+}
+
+int sort3() {
+	int a, b, c;
+	cout << "a, b, c = "; cin >> a >> b >> c;
+
+	if (a > b) {
+		int x = a;
+		a = b;
+		b = x;
+	}
+	// a <= b
+	if (b > c) {
+		int x = b;
+		b = c;
+		c = x;
+	}
+	// b <= c && (a <= b || a <= c)
+	if (a > b) {
+		int x = a;
+		a = b;
+		b = x;
+	}
+	// a <= b && b <= c
+	// => a <= c
+
+	cout << "a, b, c = " << a << ' ' << b << ' ' << c << endl;
+
+	return 0;
+}
+
+int switching() {
+	int x;
+	cout << "x = "; cin >> x;
+	switch (x) {
+		case 1 : x++; break;
+		case 2 : x += 2; break;
+		default : x += 5;
+	}
+	cout << "x = " << x << endl;
+}
+
+int calculator() {
+	int a, b;
+	char op;
+	bool error = false;
+	cin >> a >> op >> b;
+	int result = 0;
+	switch (op) {
+	case '+':result = a + b;break;
+	case '-':result = a - b;break;
+	case '*':result = a * b;break;
+	case '/':result = a / b;break;
+	case '%':result = a % b;break;
+	/*
+	 * !!!
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	case '%':cout << "Резултат: " << result << endl;
+	*/
+	default:cout << "Грешна операция!" << endl;
+			error = true;
+	}
+	if (!error)
+		cout << "Резултат: " << result << endl;
+}
+
 int main() {
 	// statements();
-	lineq();
+	// lineq();
+	// numbers3();
+	// min3();
+	// sort3();
+	// switching();
+	// calculator();
 	return 0;
 }
 
