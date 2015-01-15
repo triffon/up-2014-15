314556ce223b92fa8981406fcaded258f1054ae4
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2015-01-15 14:35:00 +0200

    Намиране на първоначалната позиция.

--- labyrinth/labyrinth.cpp
+++ labyrinth/labyrinth.cpp
@@ -25,9 +25,23 @@ void printLabyrinth() {
 		cout << labyrinth[i] << endl;
 }
 
+void findStart(int& x, int& y) {
+	for(int i = 0; i < m; i++)
+		for(int j = 0; j < n; j++)
+			if (labyrinth[i][j] == 'o') {
+				x = i;
+				y = j;
+				return;
+			}
+}
+
 int main() {
 	readLabyrinth();
 	printLabyrinth();
+	int startx, starty;
+	findStart(startx, starty);
+	cout << "Започваме от " << "("
+			<< startx << "," << starty << ")\n";
 	return 0;
 }
 
