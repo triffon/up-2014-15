98b4492917dd4d5e542529145a2bae924a6bfc69
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2015-01-22 13:00:00 +0200

    Примери за използване на записи.

---
+++ struct/struct.cpp
@@ -0,0 +1,80 @@
+/*
+ * struct.cpp
+ *
+ *  Created on: 22.01.2015 г.
+ *      Author: trifon
+ */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int const MAX = 100;
+
+struct Student;
+
+Student* p = NULL;
+
+struct Student {
+	char name[MAX];
+	int fn;
+	double grade;
+	int year;
+};
+
+struct Team {
+	Student s1, s2;
+	char name[MAX];
+};
+
+void printStudent(Student const& s) {
+	cout << "Име:    " << s.name << endl;
+	cout << "Ф№:     " << s.fn << endl;
+	cout << "Оценка: " << s.grade << endl;
+	// !!! s.grade += 1;
+	cout << endl;
+}
+
+void printTeam(Team const& t) {
+	cout << "Екип " << t.name << endl;
+	cout << "---------------------------\n";
+	cout << "Студент №1\n";
+	printStudent(t.s1);
+	cout << "Студент №2\n";
+	printStudent(t.s2);
+}
+
+void testStruct() {
+	cout << sizeof(char) << ' ' << sizeof(int) <<
+			' ' << sizeof(double) << endl;
+	cout << sizeof(Student) << endl;
+
+	Student s1 = { "Иван Колев", 44444, 4.5 };
+	cout << s1.name << endl;
+
+	Student s2;
+	s2 = s1;
+	cout << s2.name << endl;
+
+	Student s[10] = { s1, { "Петър Петров", 80000, 6 }};
+	strcpy(s[1].name, "Георги Георгиев");
+	for(int i = 0; i < 10; i++)
+		cout << s[i].name << "->" << s[i].fn << endl;
+
+	Team t = { s[1], { "Мария Иванова", 40000, 5.5 }, "GM" };
+	cout << t.s1.name << endl;
+	cout << t.name << endl;
+	t.s2.grade += 0.5;
+
+	printStudent(s1);
+	printStudent(s[1]);
+	// printStudent(s[1]);
+	printStudent(t.s1);
+
+	printTeam(t);
+}
+
+int main() {
+	testStruct();
+	return 0;
+}
