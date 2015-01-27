c0bac8099a67f18496c04e09e41b7a142761a3ef
rgeorgiev583 <rgeorgiev583@gmail.com>
2015-01-27 23:25:28 +0200

    added exercises from sixth practicum session

---
+++ 6/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int count(int n)
+{
+    if (n == 0)
+        return 1;
+
+    for (int i = 0; i <= 9; i++)
+    {
+        while (i % )
+        {
+
+        }
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
