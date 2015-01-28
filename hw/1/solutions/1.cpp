881d5f5be4f655cedc4bca976464cbadb0e44bfb
rgeorgiev583 <rgeorgiev583@gmail.com>
2015-01-28 04:53:25 +0200

    added solution to the first task from the common group (not tested yet)

--- hw/1/solutions/1.cpp
+++ hw/1/solutions/1.cpp
@@ -16,4 +16,58 @@
  * =====================================================================================
  */
 
+#include <iostream>
+using namespace std;
 
+int fact(int n)
+{
+    int P = 1;
+
+    for (int i = 2; i <= n; i++)
+        P *= i;
+
+    return P;
+}
+
+bool areEqual(int n, int m)
+{
+    double S = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int P = 1;
+
+        for (int j = i; j <= i + m; j++)
+            P *= j;
+
+        S += 1 / (double) P;
+    }
+
+    int P = 1;
+
+    for (int i = n + 1; i <= n + m; i++)
+        P *= i;
+
+    return S == 1 / (double) m * (1 / (double) fact(m) - 1 / (double) P);
+}
+
+bool areEqualForAll(int a, int b)
+{
+    for (int i = 1; i <= a; i++)
+        for (int j = 1; j <= b; j++)
+            if (!areEqual(i, j))
+                return false;
+
+    return true;
+}
+
+int main()
+{
+    int n, m;
+
+    cin >> n >> m;
+    cout << (areEqualForAll(n, m) ? "Yes" : "No");
+
+    return 0;
+}
+    
